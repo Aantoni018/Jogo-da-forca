ec60ab9e8d9f15051accc00966174f08766ca8a4
Aantoni018 <antoniocrf.info@gmail.com>
2021-06-20 19:01:29 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,118 @@
+/*
+Descrição:
+No código abaixo de javascript conta-se informações e 
+instruções para a execução de um jogo da forca, contendo
+estruturas de códigos com palavras para o jogo, número de 
+chances, condições de acertos e erros com o exibimento da
+palavra ao final de cada situação, além da implementação de
+imagens a cada letra errada que o usuário escolher.
+
+Aluno:
+Antônio Carlos Ramos Filho
+
+Data:
+20/06/2021
+
+*/
+
+let palavras = ["tecnologia", "computador", "slide", "programa", "web", "aplicativo", 
+                "teclado", "desenvolvimento", "estabilizador", "internet", "javascript", 
+                "html", "css", "notebook"];
+
+let palavra = palavras[Math.floor(Math.random() * palavras.length)];
+
+let chances = 6;
+let acertos = 0;
+
+let imagem = 0;
+
+let posicao;
+
+for (posicao = 0; posicao < palavra.length; posicao++) {
+    let span = document.createElement("span");
+    span.setAttribute('id', posicao);
+
+    let div = document.getElementById("palavra");
+    div.appendChild(span);
+}
+
+let alfabeto = "abcdefghijklmnopqrstuvwxyz";
+let letras = alfabeto.split("");
+
+for (posicao = 0; posicao < letras.length; posicao++) {
+    let botao = document.createElement("button");
+    let letra = document.createTextNode(letras[posicao]);
+    
+    botao.appendChild(letra);
+    botao.setAttribute('onclick', 'escolheLetra(\''+letras[posicao]+'\')');
+    botao.setAttribute('id', letras[posicao]);
+
+    let div = document.getElementById("letras");
+    div.appendChild(botao);
+}
+
+function escolheLetra(letra) {
+
+    let acertou = false;
+
+    for (posicao = 0; posicao < palavra.length; posicao++) {
+        if (letra === palavra[posicao]) {
+            let span = document.getElementById(posicao);
+            let l = document.createTextNode(letra);
+
+            span.appendChild(l);
+
+            let botao = document.getElementById(letra);
+            botao.setAttribute('class', 'certa');
+            botao.removeAttribute('onclick');
+
+            acertos++;
+            acertou = true;
+        }
+    }
+
+    if (acertou === false) {
+        imagem++;
+        document.getElementById("forca").src = "imagens/forca"+imagem+".jpg";
+
+        var botao = document.getElementById(letra);
+        botao.setAttribute('class', 'errada');
+        botao.removeAttribute('onclick');
+
+        chances--;
+    }
+
+    if (chances === 0) {
+        let mensagem = document.createElement("p");
+        let t1 = document.createTextNode("Você perdeu! A palavra era: " + palavra);
+        mensagem.appendChild(t1);
+
+        let botao = document.createElement("button");
+        let t2 = document.createTextNode("jogar novamente");
+        
+        botao.appendChild(t2);
+        botao.setAttribute('class', 'novo-bt');
+        botao.setAttribute('onclick', 'window.location.reload()');
+
+        let div = document.getElementById("novo");
+        div.appendChild(mensagem);
+        div.appendChild(botao);
+    }
+
+    if (acertos === palavra.length) {
+        let mensagem = document.createElement("p");
+        let t1 = document.createTextNode("Você venceu!");
+        mensagem.appendChild(t1);
+
+        let botao = document.createElement("button");
+        let t2 = document.createTextNode("jogar novamente");
+        
+        botao.appendChild(t2);
+        botao.setAttribute('class', 'novo-bt');
+        botao.setAttribute('onclick', 'window.location.reload()');
+
+        let div = document.getElementById("novo");
+        div.appendChild(mensagem);
+        div.appendChild(botao);
+    }
+}
